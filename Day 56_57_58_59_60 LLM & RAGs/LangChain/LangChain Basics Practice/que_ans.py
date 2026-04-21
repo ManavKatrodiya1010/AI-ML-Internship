1da93c9df24acc2ad7e868b3f0dc7208ddc50b56
Manav Katrodiya <manavkatrodiya10@gmail.com>
2026-04-21 12:41:38 +0530

    commit

---
+++ Day 56_57_58_59_60 LLM & RAGs/LangChain/LangChain Basics Practice/que_ans.py
@@ -0,0 +1,6 @@
+from langchain_model import que_ans
+
+question = input("Question :- ")
+answer = que_ans(question)
+
+print(answer)
--- Day 56_57_58_59_60 LLM & RAGs/langchain/que_ans.py
+++
@@ -1,6 +0,0 @@
-from langchain_model import que_ans
-
-question = input("Please enter your question :- ")
-answer = que_ans(question)
-
-print(answer)
